8df8bdfdcf6a8ab094409bab23382ec57eed236e
ChrissSav <dai17023@uom.edu.gr>
2021-08-30 19:49:18 +0300

    Update login page add material UI components

--- FrontEnd/src/components/Login/Login.js
+++ FrontEnd/src/components/Login/Login.js
@@ -6,6 +6,10 @@ import { useDispatch } from 'react-redux';
 import { userLogin } from '../../redux/actions/userLogin';
 import axiosApp from '../Util/axiosApp';
 import Statics from '../Util/Statics';
+import PasswordField from 'material-ui-password-field';
+import { FormControl } from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
 
 const Login = () => {
   const [email, setEmail] = useState('firstNadme@gmail.com');
@@ -61,29 +65,33 @@ const Login = () => {
         }}
         onSubmit={onSubmit}
       >
-        <div className='form-control'>
-          <label>Email</label>
-          <input
-            type='email'
-            required='required'
-            placeholder='UserName'
+        <div>
+          <TextField
+            id='standard-basic'
+            required
+            className='login-form-input'
+            style={{ width: '100%', marginBottom: '60px' }}
+            label='Email'
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
-        </div>
-        <div className='form-control'>
-          <label>Password</label>
-          <input
-            type='password'
-            placeholder='password'
-            value={password}
-            required='required'
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
+          <FormControl
+            required
+            style={{ width: '100%', marginBottom: '100px' }}
+          >
+            <InputLabel htmlFor='standard-adornment-password'>
+              Password
+            </InputLabel>
+            <PasswordField
+              value={password}
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </FormControl>
         </div>
 
         <input
